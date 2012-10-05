04082cea3e72744d6f490615c6a48c1f7c19ea40
Jeremy Olliver <jeremy.olliver@gmail.com>
2012-10-05 23:19:53 +1300

    Adding project specific knife file

---
+++ .chef/knife.rb
@@ -0,0 +1,22 @@
+current_dir = File.dirname(__FILE__)
+repo_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+user = ENV['CHEF_USER'] || ENV['USER']
+
+log_level                :info
+log_location             STDOUT
+node_name                user
+client_key               "#{ENV['HOME']}/.chef/#{user}.pem"
+validation_client_name   'chef-validator'
+validation_key           "#{repo_root}/.chef/validation.pem"
+# ssl_verify_mode           :verify_none
+chef_server_url          'http://chef.example.com:4000'
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks", "#{current_dir}/../custom-cookbooks"]
+
+cookbook_copyright       "ORGANISATION NAMW"
+cookbook_email           "admin@ORGANISATION.com"
+
+bootstrap_version        "10.14.4"
+
+Mixlib::Log::Formatter.show_time = true
